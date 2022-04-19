228052ac1d015d2ab17131ff3c2f8332fecdd30b
Fung Chow <fungchow@gmail.com>
2022-04-20 00:25:14 +0100

    add method isWinner to check whether player has won the game.

--- src/main/java/com/techreturner/game/Tictactoe.java
+++ src/main/java/com/techreturner/game/Tictactoe.java
@@ -2,12 +2,15 @@ package com.techreturner.game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tictactoe {
 
     private final int minimumBoxNum = 1;
     private final int maximumBoxNum = 9;
     private final int ELEMENTPERLINE = 5;
+    private final String playerASymbol = "X";
+    private final String playerBSymbol = "O";
     private String gameBoard[] = {"1", "|", "2", "|", "3",
                                   "-", "+", "-", "+", "-",
                                   "4", "|", "5", "|", "6",
@@ -16,8 +19,16 @@ public class Tictactoe {
     private ArrayList<Integer> availableBox = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
     private ArrayList<Integer> playerASelected = new ArrayList<Integer>();
     private ArrayList<Integer> playerBSelected = new ArrayList<Integer>();
+    private final Integer [][] winningNumberlist = {{1,2,3},{4,5,6},{7,8,9},
+                                                {1,4,7},{2,5,8},{3,6,9},
+                                                {1,5,9},{3,5,7}
+                                                };
 
-    public String displayGameBoard(){
+    public void play(){
+
+    }
+
+    public String printGameBoard(){
         String board = "";
         for (int i=0; i<gameBoard.length; i++) {
             System.out.print(gameBoard[i]);
@@ -30,13 +41,19 @@ public class Tictactoe {
         return board;
     }
 
+    public boolean isWinner(ArrayList<Integer> playerSelected, Integer [] winningNumber){
+        if (playerSelected.size()<3)
+            return false;
+        return playerSelected.containsAll(new ArrayList<Integer>(List.of(winningNumber)));
+    }
+
     public Boolean isAvailableBox(int boxNum){
         if (boxNum >= minimumBoxNum && boxNum <= maximumBoxNum && availableBox.indexOf(boxNum) != -1)
             return true;
         return false;
     }
 
-    public int getIndex(int boxNumber){
+    private int getIndex(int boxNumber){
         return switch (boxNumber) {
             case 1 -> 0;
             case 2 -> 2;
@@ -47,31 +64,33 @@ public class Tictactoe {
             case 7 -> 20;
             case 8 -> 22;
             case 9 -> 24;
-            default -> 0;
+            default -> throw new IllegalArgumentException();
         };
     }
 
-    public void addItemToBoard(int boxNumber){
+    public void addItemToBoard(int boxNumber, String symbol){
         if (isAvailableBox(boxNumber)){
-            gameBoard[getIndex(boxNumber)] = "X";
-            availableBox.remove(new Integer(boxNumber));
+            gameBoard[getIndex(boxNumber)] = symbol;
+            availableBox.remove(Integer.valueOf(boxNumber));
         }else
             System.out.println("Box " + boxNumber + " is not available.");
 
     }
 
     public void printAvailableBox(){
-        System.out.println(availableBox.toString());
+        System.out.println(availableBox);
     }
 
     public static void main(String[] args) {
         Tictactoe a = new Tictactoe();
-        a.addItemToBoard(1);
-        a.addItemToBoard(3);
-        a.addItemToBoard(9);
-        a.addItemToBoard(9);
+        a.addItemToBoard(1, "X");
+        a.addItemToBoard(3, "O");
+        a.addItemToBoard(9, "X");
+        a.addItemToBoard(9, "O");
         a.printAvailableBox();
-        a.displayGameBoard();
+        a.printGameBoard();
+        Integer [] b = {1,2,3};
+        System.out.println(a.isWinner(new ArrayList<Integer>(Arrays.asList(1,2,5,7,3)), b));
     }
 
 }
--- src/test/java/com/techreturner/game/TictactoeTest.java
+++ src/test/java/com/techreturner/game/TictactoeTest.java
@@ -10,50 +10,50 @@ public class TictactoeTest {
 
     @Test
     public void checkDisplayGameBoardTest(){
-        assertEquals("1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.displayGameBoard());
+        assertEquals("1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.printGameBoard());
     }
 
     @Test
     public void checkAddItemToBoard(){
-        game.addItemToBoard(1);
-        assertEquals("X|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(2);
-        assertEquals("X|X|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(3);
-        assertEquals("X|X|X\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(4);
-        assertEquals("X|X|X\n-+-+-\nX|5|6\n-+-+-\n7|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(5);
-        assertEquals("X|X|X\n-+-+-\nX|X|6\n-+-+-\n7|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(6);
-        assertEquals("X|X|X\n-+-+-\nX|X|X\n-+-+-\n7|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(7);
-        assertEquals("X|X|X\n-+-+-\nX|X|X\n-+-+-\nX|8|9\n", game.displayGameBoard());
-        game.addItemToBoard(8);
-        assertEquals("X|X|X\n-+-+-\nX|X|X\n-+-+-\nX|X|9\n", game.displayGameBoard());
-        game.addItemToBoard(9);
-        assertEquals("X|X|X\n-+-+-\nX|X|X\n-+-+-\nX|X|X\n", game.displayGameBoard());
+        game.addItemToBoard(1, "X");
+        assertEquals("X|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.printGameBoard());
+        game.addItemToBoard(2, "O");
+        assertEquals("X|O|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.printGameBoard());
+        game.addItemToBoard(3, "X");
+        assertEquals("X|O|X\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", game.printGameBoard());
+        game.addItemToBoard(4, "O");
+        assertEquals("X|O|X\n-+-+-\nO|5|6\n-+-+-\n7|8|9\n", game.printGameBoard());
+        game.addItemToBoard(5, "X");
+        assertEquals("X|O|X\n-+-+-\nO|X|6\n-+-+-\n7|8|9\n", game.printGameBoard());
+        game.addItemToBoard(6, "O");
+        assertEquals("X|O|X\n-+-+-\nO|X|O\n-+-+-\n7|8|9\n", game.printGameBoard());
+        game.addItemToBoard(7, "X");
+        assertEquals("X|O|X\n-+-+-\nO|X|O\n-+-+-\nX|8|9\n", game.printGameBoard());
+        game.addItemToBoard(8, "O");
+        assertEquals("X|O|X\n-+-+-\nO|X|O\n-+-+-\nX|O|9\n", game.printGameBoard());
+        game.addItemToBoard(9, "X");
+        assertEquals("X|O|X\n-+-+-\nO|X|O\n-+-+-\nX|O|X\n", game.printGameBoard());
     }
 
     @Test
     public void checkAvailableBoxtest(){
-        game.addItemToBoard(1);
+        game.addItemToBoard(1, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(1));
-        game.addItemToBoard(3);
+        game.addItemToBoard(3, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(3));
-        game.addItemToBoard(6);
+        game.addItemToBoard(6, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(6));
-        game.addItemToBoard(2);
+        game.addItemToBoard(2, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(2));
-        game.addItemToBoard(5);
+        game.addItemToBoard(5, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(5));
-        game.addItemToBoard(8);
+        game.addItemToBoard(8, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(8));
-        game.addItemToBoard(9);
+        game.addItemToBoard(9, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(9));
-        game.addItemToBoard(7);
+        game.addItemToBoard(7, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(7));
-        game.addItemToBoard(4);
+        game.addItemToBoard(4, "X");
         assertFalse("this is not valid box number", game.isAvailableBox(4));
     }
 
